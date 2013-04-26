3d5c6ce801aea10b7953a6977e7bc7c247ae4ac5
saito <yosuke.saito1202@gmail.com>
2013-04-26 17:28:20 +0900

    その他完成．トップは「済」の画像がまだだったorz

--- Resources/include/Others.js
+++ Resources/include/Others.js
@@ -16,123 +16,136 @@
 			window : othersWin
 		});
 
-                var row1 = Ti.UI.createTableViewRow();
-                row1.height = 65;
+		var row1 = Ti.UI.createTableViewRow();
+		row1.height = 65;
 
-                var title1 = Ti.UI.createLabel({
-                        font:{fontSize:20, fontWeight:'bold'},
-                        left:15,
-                        top:5,
-                        height:30,
-                        width:200,
-                        text:'ヘルプ'
-                    });
+		var title1 = Ti.UI.createLabel({
+			font : {
+				fontSize : 20,
+				fontWeight : 'bold'
+			},
+			left : 15,
+			top : 5,
+			height : 30,
+			width : 200,
+			text : 'ヘルプ'
+		});
 
-                row1.add(title1);
+		row1.add(title1);
 
-                var subtitle1 = Ti.UI.createLabel({
-                        color: '#808080',
-                        font:{fontSize:15},
-                        left:15,
-                        top:30,
-                        height:30,
-                        width:200,
-                        text:'スタンプラリーの説明です'
-                    });
+		var subtitle1 = Ti.UI.createLabel({
+			color : '#808080',
+			font : {
+				fontSize : 15
+			},
+			left : 15,
+			top : 30,
+			height : 30,
+			width : 200,
+			text : 'スタンプラリーの説明です'
+		});
 
-                row1.add(subtitle1);
+		row1.add(subtitle1);
 
-                row1.addEventListener('click', function(e){
-                    Titanium.UI.createAlertDialog({
-                        title: 'スタンプラリーの説明',
-                        message: '京都が誇るラーメン街、一乗寺を回るスタンプラリーです！\n' +
-                                 '2013/4/8(月)〜7/31(水)の間にスタンプラリー対象店舗10店の' +
-                                 'ラーメンを食べ、各店に貼ってあるQRコードをアプリのバーコード' +
-                                 'リーダーで取ると、スタンプをGETできます。\n' +
-                                 '全店のスタンプを集めると、1週間有効なお得なクーポンが発行されます。\n' +
-                                 'この機会に一乗寺のラーメンを食べに行こう！',
-                    }).show();
-                });
+		row1.addEventListener('click', function(e) {
+			Titanium.UI.createAlertDialog({
+				title : 'スタンプラリーの説明',
+				message : '京都が誇るラーメン街、一乗寺を回るスタンプラリーです！\n' + '2013/4/8(月)〜7/31(水)の間にスタンプラリー対象店舗10店の' + 'ラーメンを食べ、各店に貼ってあるQRコードをアプリのバーコード' + 'リーダーで取ると、スタンプをGETできます。\n' + '全店のスタンプを集めると、1週間有効なお得なクーポンが発行されます。\n' + 'この機会に一乗寺のラーメンを食べに行こう！',
+			}).show();
+		});
 
-                var row2 = Ti.UI.createTableViewRow();
-                row2.height = 65;
+		var row2 = Ti.UI.createTableViewRow();
+		row2.height = 65;
 
-                var title2 = Ti.UI.createLabel({
-                        font:{fontSize:20, fontWeight:'bold'},
-                        left:15,
-                        top:5,
-                        height:30,
-                        width:200,
-                        text:'Facebookページ'
-                    });
+		var title2 = Ti.UI.createLabel({
+			font : {
+				fontSize : 20,
+				fontWeight : 'bold'
+			},
+			left : 15,
+			top : 5,
+			height : 30,
+			width : 200,
+			text : 'Facebookページ'
+		});
 
-                row2.add(title2);
+		row2.add(title2);
 
-                var subtitle2 = Ti.UI.createLabel({
-                        color: '#808080',
-                        font:{fontSize:15},
-                        left:15,
-                        top:30,
-                        height:30,
-                        width:300,
-                        text:'アプリのFacebookページへとびます'
-                    });
+		var subtitle2 = Ti.UI.createLabel({
+			color : '#808080',
+			font : {
+				fontSize : 15
+			},
+			left : 15,
+			top : 30,
+			height : 30,
+			width : 300,
+			text : 'アプリのFacebookページへとびます'
+		});
 
-                row2.add(subtitle2);
+		row2.add(subtitle2);
 
-                row2.addEventListener('click', function(e){
-                        Ti.Platform.openURL('http://www.facebook.com/IchijojiRamen');
-                });
+		row2.addEventListener('click', function(e) {
+			Ti.Platform.openURL('http://www.facebook.com/IchijojiRamen');
+		});
 
-                var row3 = Ti.UI.createTableViewRow();
-                row3.height = 65;
+		var row3 = Ti.UI.createTableViewRow();
+		row3.height = 65;
 
-                var title3 = Ti.UI.createLabel({
-                        font:{fontSize:20, fontWeight:'bold'},
-                        left:15,
-                        top:5,
-                        height:30,
-                        width:200,
-                        text:'リセット'
-                    });
+		var title3 = Ti.UI.createLabel({
+			font : {
+				fontSize : 20,
+				fontWeight : 'bold'
+			},
+			left : 15,
+			top : 5,
+			height : 30,
+			width : 200,
+			text : 'リセット'
+		});
 
-                row3.add(title3);
+		row3.add(title3);
 
-                var subtitle3 = Ti.UI.createLabel({
-                        color: '#808080',
-                        font:{fontSize:15},
-                        left:15,
-                        top:30,
-                        height:30,
-                        width:200,
-                        text:'記録を消去します'
-                    });
+		var subtitle3 = Ti.UI.createLabel({
+			color : '#808080',
+			font : {
+				fontSize : 15
+			},
+			left : 15,
+			top : 30,
+			height : 30,
+			width : 200,
+			text : '記録を消去します'
+		});
 
-                row3.add(subtitle3);
+		row3.add(subtitle3);
 
-                row3.addEventListener('click', function(e){
-                    var alert = Titanium.UI.createAlertDialog({
-                        title: 'リセットしますか？',
-                        message: '今までの記録がなくなってしましますが、よろしいですか？',
-                        buttonNames: ['はい','いいえ'],
-                        cancel: 1,
-                    });
-                    alert.addEventListener('click',function(e){
-                            if(e.index == 0){
-                                var db = new RamenDatabase();
-                                // データベースのフラグ消去
-                                // アプリのプロパティも消去
-                                db.close();
-                            }
-                    });
-                    alert.show();
-                });
+		row3.addEventListener('click', function(e) {
+			var alert = Titanium.UI.createAlertDialog({
+				title : 'リセットしますか？',
+				message : '今までの記録がなくなってしましますが、よろしいですか？',
+				buttonNames : ['はい', 'いいえ'],
+				cancel : 1,
+			});
+			alert.addEventListener('click', function(e) {
+				if (e.index == 0) {
+					var db = new RamenDatabase();
+					// フラグ消去
+					db.deleteFlags();
+					// プロパティ消去
+					Ti.App.Properties.setString('startTime', null);
+					Ti.App.Properties.setString('endtime', null);
+					db.close();
+				}
+			});
+			alert.show();
+		});
 
-                var rows = [row1, row2, row3];
+		var rows = [row1, row2, row3];
 
-                var tableView = Titanium.UI.createTableView({
-                    data: rows
-                    });
+		var tableView = Titanium.UI.createTableView({
+			data : rows
+		});
 
 		othersWin.add(tableView);
 		return tab;
